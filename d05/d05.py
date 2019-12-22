a1f51d8721c245d34949274cf224ca9b8beff16f
bja <bja@drivexpert.de>
2019-12-22 17:30:16 +0100

    day5 prepared

---
+++ d05/d05.py
@@ -0,0 +1,23 @@
+
+class d04:
+    def __init__(self):
+        self.data = []
+
+
+    def readData(self):
+        # print('data read')
+        # with open('./d03/input.txt') as f:
+        #     self.line1 = f.readline().split(",")
+        #     self.line2 = f.readline().split(",")
+        print('data read')
+
+
+    def solvePartOne(self):
+        return True
+
+
+    def solvePartTwo(self):
+        return True
+
+
+    
